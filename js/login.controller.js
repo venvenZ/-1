c11a9589327a724f902121757d73f9aa98b31e6e
venvenZ <zhangying474831652@qq.com>
2017-12-24 23:19:08 +0800

    zhangying

--- js/login.controller.js
+++ js/login.controller.js
@@ -5,7 +5,9 @@
         .controller('login.Controller',function ($scope,$state) {
           $scope.stateGo=function () {
               $state.go('app.introduce');
-              window.location.href='libs/WEB前端开发--张颖.docx'
           }
+            $scope.down=function () {
+                window.location.href='libs/WEB前端开发--张颖.docx'
+            }
         })
 })();
